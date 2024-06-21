5825959359755f8904c0a25abfb35b6cb8e6ed35
Mohammed Adam <adamekissa@gmail.com>
2024-06-21 10:11:39 +0100

    length check added

---
+++ src/anagram.js
@@ -0,0 +1,27 @@
+function anagram(string1, string2) {
+    if (string1.length !== string2.length) {
+        return false;
+    }
+}
+
+
+export { anagram }
+
+
+
+
+
+
+
+// Function anagram (STRING11, STRING2){
+
+//     check if both STRINGS length, if not equal
+//          Return false
+//     Make both string1 and string2 lower case
+//     split both strings by an empty string in to two separate arrays
+//     sort both arrays alphabetically
+//     Loop through 
+//         IF array1[i] is NOT equal array2[i]
+//             RETURN false
+//     RETURN true
+//     }
--- src/anagram.test.js
+++ src/anagram.test.js
@@ -1,2 +1,29 @@
-import jest from 'jest/global'
+import { anagram } from './anagram';
 
+test("Test return true for empty strings", () => {
+    const string1 = "";
+    const string2 = "";
+    const expected = true;
+    expect(anagram(string1, string2)).toBe(expected)
+});
+
+test("Test returns false if strings are different lengths", () => {
+    const string1 = "abc";
+    const string2 = "abcd";
+    const expected = false;
+    expect(anagram(string1, string2)).toBe(expected)
+});
+
+test("Test case insensitive", () => {
+    const string1 = "AbCd";
+    const string2 = "aBcD";
+    const expected = true;
+    expect(anagram(string1, string2)).toBe(expected)
+});
+
+test("Test fails for non-anagram", () => {
+    const string1 = "apple";
+    const string2 = "bpple";
+    const expected = false;
+    expect(anagram(string1, string2)).toBe(expected)
+});
